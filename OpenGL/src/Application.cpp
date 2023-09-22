1c933fe0ad327e9c701d725e1a40c2150590ca05
ayanchavand <ayan.chavand15@gmail.com>
2023-09-22 15:20:34 +0530

    Drawing the triangle using default shader and modern Open GL

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -27,6 +27,29 @@ int main(void)
     }
     else
         std::cout << "WOW IT WORKS, ALSO IT LOOKS COOL TO ADD CONSOLE PRINT STATEMENTS HAHA" << std::endl;
+        
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    
+    //positions of the triangle
+    float positions[6] = {
+        -0.5, -0.5,
+        0.0, 0.5,
+        0.5, -0.5
+    };
+    
+    //declaration of an unsigned int whose address is used as the buffer id
+    unsigned int buffer;
+
+    //generated a buffer, takes argument as the size of the buffer and buffer id
+    glGenBuffers(1, &buffer);
+    //selects the above buffer and usses it as buffer for vertex data
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    //creates a memory allocation in the GPU for the sleceted buffer 
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    //enables use of vertex attributes from the buffer
+    glEnableVertexAttribArray(0);
+    //defines the vertext attributes and how should they be interpreted for rendering
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void*)0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -34,11 +57,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(0.0, 0.5);
-        glVertex2f(0.5, -0.5);
-
+        //Draw call for the bound/selected buffer
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
