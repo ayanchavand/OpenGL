333543db82eca1552ec7fd0c68ff0cd5d01e2d80
ayanchavand <ayan.chavand15@gmail.com>
2023-09-23 10:07:37 +0530

    added compile shader and create shader functions, added a basic shader

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,9 +1,62 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include <GL/glew.h>    //finds the drivers and gets us access to OpenGL functions accordingly
+#include <GLFW/glfw3.h> //used to open windows in a partciular os (used for cross platform compatibility)
 #include <iostream>
 
-int main(void)
-{
+
+static unsigned int CompileShader(unsigned int type, const std::string& source){   
+
+    //creates empty shader object
+    unsigned int id = glCreateShader(type);
+    //pointer to the first byte of the source code
+    const char* src = source.c_str();
+    //places the source code in the given shader object
+    glShaderSource(id, 1, &src, nullptr);
+    //compiles the given shader
+    glCompileShader(id);
+
+    int result;
+    //gets the given param value and adds it to the given result 
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+    //error handling in case the compilation fails
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char)); glGetShaderInfoLog(id, length, &length, message);
+
+        std::cout << "Failed to compile" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+        std::cout << message << std::endl;
+
+        glDeleteShader(id);
+        return 0;
+
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    
+    //creates a program object and compiles the given shaders
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    //linkes the shaders to the proogram
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    //deletes the shader as they have been attached to the object
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+int main(void){
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -50,6 +103,24 @@ int main(void)
     glEnableVertexAttribArray(0);
     //defines the vertext attributes and how should they be interpreted for rendering
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void*)0);
+
+    //source code for vs and fs shaders
+    std::string vs = "#version 330 core\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "};";
+
+    std::string fs = "#version 330 core\n"
+        "layout (location = 0) out vec4 color;\n"
+        "void main()\n"
+        "{\n"
+        "    color = vec4(1, 0, 0, 1);\n"
+        "};";
+
+    unsigned int shader = CreateShader(vs, fs);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
